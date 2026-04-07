75a6a9548cd33eb1e92f44ee0fdccc5cea39a1ec
abdelrahman-anwar-tech <abdelrahman3755@gmail.com>
2026-04-07 19:44:06 +0200

    Add missing includes for standard libraries in shared header

--- shared.h
+++ shared.h
@@ -2,6 +2,8 @@
 #define SHARED_H
 
 #include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define MSG_KEY       1234   /* generator  →  scheduler message queue  */
 #define SHM_KEY_CLK   4321   /* clock shared memory  (used by clk.c)   */
